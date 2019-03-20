5685df98a8cfa569c1a0dc59e10a07a1c3c7c740
Whazbakh <morten.eggert.fuglsang@gmail.com>
2019-03-20 12:23:25 +0100

    Working on rollback

--- consensus/Skoven.go
+++ consensus/Skoven.go
@@ -9,47 +9,21 @@ var unusedTransactions map[string]bool
 var transactions map[string]o.Transaction
 var tLock sync.RWMutex
 var blocks skov
+var badblocks map[string]bool
 var currentHead string
 var currentLength int
 var lastFinalized string
 
 //Calculates and compares pathWeigth of currentHead and a new block not extending the tree of the head.
 // Updates the head and initiates rollbacks accordingly
-type skov struct {
-	m map[string]o.Block
-	l sync.RWMutex
-}
-
-func (s *skov) add(block o.Block) {
-	hash := block.HashBlock()
-	s.m[hash] = block
-}
-
-func (s *skov) get(blockHash string) o.Block {
-	return s.m[blockHash]
-}
-
-func (s *skov) lock() {
-	s.l.Lock()
-}
-
-func (s *skov) unlock() {
-	s.l.Unlock()
-}
-
-func (s *skov) rlock() {
-	s.l.RLock()
-}
-
-func (s *skov) runlock() {
-	s.l.RUnlock()
-}
-
 func comparePathWeight(b o.Block) {
 	len := 1
 	for {
+		if !blocks.contains(b.ParentPointer) {
+			return
+		}
 		parent := blocks.get(b.ParentPointer)
-		// TODO: case on nil
+
 		if parent.Slot == 0 { // *TODO Should probably refactor to use the last finalized block, to prevent excessive work
 			break
 		}
@@ -60,24 +34,47 @@ func comparePathWeight(b o.Block) {
 	}
 
 	if len > currentLength {
-		rollback()
+		rollback(b)
 		currentHead = b.HashBlock()
 		currentLength = len
 		return
 	}
 
 	if calculateDraw(blocks.get(currentHead)) < calculateDraw(b) {
-		rollback()
+		rollback(b)
 		currentHead = b.HashBlock()
 	}
 }
 
 //Manages rollback in the case of branch shifting
-func rollback() {
-	//* TODO
+func rollback(newHead o.Block) {
+	tLock.Lock()
+	defer tLock.Unlock()
+	newUnusedTransmap := make(map[string]bool)
+	for k, v := range unusedTransactions {
+		newUnusedTransmap[k] = v
+	}
+	head := blocks.get(currentHead)
+	for {
+		transactionsUnused(head, newUnusedTransmap)
+		head = blocks.get(head.ParentPointer)
+		if head.HashBlock() == lastFinalized {
+			break
+		}
+	}
+	//*TODO not done
+}
+
+//Helper function for rollback
+func transactionsUnused(b o.Block, utMap map[string]bool) {
+	trans := b.BlockData.Trans
+	for _, t := range trans {
+		utMap[t.ID] = true
+	}
 }
 
 //Removes the transactions used in a block from unusedTransactions, and saves transactions that we have not already saved.
+// *TODO need to add check for double spending transactions
 func transactionsUsed(b o.Block) {
 	trans := b.BlockData.Trans
 	for _, t := range trans {
@@ -136,4 +133,39 @@ func calculateDraw(b o.Block) int {
 func verifyDraw(b o.Block) bool {
 	// *TODO
 	return true
+}
+
+type skov struct {
+	m map[string]o.Block
+	l sync.RWMutex
+}
+
+func (s *skov) add(block o.Block) {
+	hash := block.HashBlock()
+	s.m[hash] = block
+}
+
+func (s *skov) get(blockHash string) o.Block {
+	return s.m[blockHash]
+}
+
+func (s *skov) contains(blockHash string) bool {
+	_, exists := s.m[blockHash]
+	return exists
+}
+
+func (s *skov) lock() {
+	s.l.Lock()
+}
+
+func (s *skov) unlock() {
+	s.l.Unlock()
+}
+
+func (s *skov) rlock() {
+	s.l.RLock()
+}
+
+func (s *skov) runlock() {
+	s.l.RUnlock()
 }
