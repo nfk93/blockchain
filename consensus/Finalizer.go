fe0bd92cc7dd9cf3ec6145fb9bd09eaec3f3f873
Whazbakh <morten.eggert.fuglsang@gmail.com>
2019-03-27 15:36:14 +0100

    Began work on finalizer

--- consensus/Finalizer.go
+++ consensus/Finalizer.go
@@ -1,9 +1,26 @@
 package consensus
 
+import "time"
+
+var slotLength int
+var currentSlot int
+
+func runSlot() {
+	for {
+		go drawLottery()
+		time.Sleep(time.Duration(slotLength) * time.Second)
+		currentSlot++
+	}
+}
+
 func finalize() {
 
 }
 
 func updateStake() {
+
+}
+
+func drawLottery() {
 
 }
