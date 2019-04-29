bf5d77b2272c27391df7b6f1f79b29cc37b9ccac
nfk93 <nfk1993@gmail.com>
2019-04-29 12:13:05 +0200

    type translation and more refactoring

--- interpreter/ast/semant.go
+++ interpreter/ast/semant.go
@@ -1,9 +1,13 @@
 package ast
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
-type TypeEnv int // TODO
-type VarEnv int  // TODO
+type TypeEnv map[string]Type         // TODO
+type VarEnv map[string]Type          // TODO
+type StructEnv map[string]StructType // TODO
 
 type TypedExp struct {
 	Exp  Exp
@@ -15,78 +19,117 @@ func (e TypedExp) String() string {
 }
 
 func InitialTypeEnv() TypeEnv {
-	return -1 // TODO
+	return make(map[string]Type) // TODO
 }
 
 func InitialVarEnv() VarEnv {
-	return -1 // TODO
+	return make(map[string]Type) // TODO
 }
 
-func todo(exp Exp) (TypedExp, VarEnv, TypeEnv) {
-	return TypedExp{exp, NotImplementedType{}}, -1, -1
+func InitialStructEnv() StructEnv {
+	return make(map[string]StructType) // TODO
+}
+
+func todo(exp Exp, venv VarEnv, tenv TypeEnv, senv StructEnv) (TypedExp, VarEnv, TypeEnv, StructEnv) {
+	return TypedExp{exp, NotImplementedType{}}, venv, tenv, senv
 }
 
 func AddTypes(exp Exp) TypedExp {
-	texp, _, _ := addTypes(exp, InitialVarEnv(), InitialTypeEnv())
+	texp, _, _, _ := addTypes(exp, InitialVarEnv(), InitialTypeEnv(), InitialStructEnv())
 	return texp
 }
 
-func addTypes(exp Exp, venv VarEnv, tenv TypeEnv) (TypedExp, VarEnv, TypeEnv) {
+func translateType(typ Type) Type {
+	switch typ.Type() {
+	case STRING, INT, FLOAT, KEY, BOOL, KOIN, OPERATION, UNIT:
+		return typ
+	case LIST:
+		typ := typ.(ListType)
+		return ListType{translateType(typ.Typ)}
+	case TUPLE:
+		typ := typ.(TupleType)
+		typ1 := translateType(typ.Typ1)
+		typ2 := translateType(typ.Typ2)
+		return TupleType{typ1, typ2}
+	case STRUCT:
+		typ := typ.(StructType)
+		fields := make([]StructField, 0)
+		for _, field := range typ.Fields {
+			fieldtyp := translateType(field.Typ)
+			fields = append(fields, StructField{field.Id, fieldtyp})
+		}
+		return StructType{fields}
+	case DECLARED:
+		return typ
+	default:
+		log.Fatal("SHOULD NOT HAPPEN")
+		return NotImplementedType{}
+	}
+}
+
+func addTypes(
+	exp Exp,
+	venv VarEnv,
+	tenv TypeEnv,
+	senv StructEnv,
+) (TypedExp, VarEnv, TypeEnv, StructEnv) {
+
 	switch exp.(type) {
 	case TopLevel:
-		return todo(exp)
+		return todo(exp, venv, tenv, senv)
 	case BinOpExp:
-		return todo(exp)
+		return todo(exp, venv, tenv, senv)
 	case TypeDecl:
+		exp := exp.(TypeDecl)
 
-		return todo(exp)
+		return todo(exp, venv, tenv, senv)
 	case EntryExpression:
-		return todo(exp)
+		return todo(exp, venv, tenv, senv)
 	case KeyLit:
-		return TypedExp{exp, KeyType{}}, venv, tenv
+		return TypedExp{exp, KeyType{}}, venv, tenv, senv
 	case BoolLit:
-		return TypedExp{exp, BoolType{}}, venv, tenv
+		return TypedExp{exp, BoolType{}}, venv, tenv, senv
 	case IntLit:
-		return TypedExp{exp, IntType{}}, venv, tenv
+		return TypedExp{exp, IntType{}}, venv, tenv, senv
 	case FloatLit:
-		return TypedExp{exp, FloatType{}}, venv, tenv
+		return TypedExp{exp, FloatType{}}, venv, tenv, senv
 	case KoinLit:
-		return TypedExp{exp, KoinType{}}, venv, tenv
+		return TypedExp{exp, KoinType{}}, venv, tenv, senv
 	case StringLit:
-		return TypedExp{exp, StringType{}}, venv, tenv
+		return TypedExp{exp, StringType{}}, venv, tenv, senv
 	case UnitLit:
-		return TypedExp{exp, UnitType{}}, venv, tenv
+		return TypedExp{exp, UnitType{}}, venv, tenv, senv
 	case StructLit:
-		return todo(exp)
+		return todo(exp, venv, tenv, senv)
 	case ListLit:
-		return todo(exp)
+		return todo(exp, venv, tenv, senv)
 	case ListConcat:
-		return todo(exp)
+		return todo(exp, venv, tenv, senv)
 	case CallExp:
-		return todo(exp)
+		return todo(exp, venv, tenv, senv)
 	case LetExp:
-		return todo(exp)
+		return todo(exp, venv, tenv, senv)
 	case AnnoExp:
-		return todo(exp)
+		return todo(exp, venv, tenv, senv)
 	case TupleExp:
-		return todo(exp)
+		return todo(exp, venv, tenv, senv)
 	case VarExp:
-		return todo(exp)
+		return todo(exp, venv, tenv, senv)
 	case ExpSeq:
-		return todo(exp)
+		return todo(exp, venv, tenv, senv)
 	case IfThenElseExp:
-		return todo(exp)
+		return todo(exp, venv, tenv, senv)
 	case IfThenExp:
-		return todo(exp)
+		return todo(exp, venv, tenv, senv)
 	case ModuleLookupExp:
-		return todo(exp)
+		return todo(exp, venv, tenv, senv)
 	case LookupExp:
-		return todo(exp)
+		return todo(exp, venv, tenv, senv)
 	case UpdateStructExp:
-		return todo(exp)
+		return todo(exp, venv, tenv, senv)
 	case StorageInitExp:
-		return todo(exp)
+		return todo(exp, venv, tenv, senv)
 	default:
-		return todo(exp)
+		return todo(exp, venv, tenv, senv)
 	}
 }
